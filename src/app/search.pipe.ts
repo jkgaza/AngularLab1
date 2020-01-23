37e20e66e015f8218f8867aa182bd0ce4954e8f6
Jordan Gaza <Gaza>
2020-01-22 20:39:55 -0500

    Got everything figured out. Thanks, Chris

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 import { ITodo } from './components/todo/todo.component';
 
 @Pipe({
-  name: "search"
+  name: "searchFilter"
 })
 export class SearchPipe implements PipeTransform {
   transform(todos: ITodo[], filterText: string): any {
